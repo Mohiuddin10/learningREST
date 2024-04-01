6d8d49948fada7ce3675758d51b2a2ba8d49ec8b
Mohiuddin10 <sj.ctg6737@gmail.com>
2024-04-01 14:57:09 +0600

    server error added

--- app.js
+++ app.js
@@ -14,5 +14,13 @@ app.get("/", (req, res) => {
 })
 
 // invalid route set 
+app.use((req, res, next) => {
+    res.status(404).json({message: "bad request"})
+})
+
+// server error 
+app.use((err, req, res, next) => {
+    res.status(500).json({message: "something broke"})
+})
 
 module.exports = app; 
